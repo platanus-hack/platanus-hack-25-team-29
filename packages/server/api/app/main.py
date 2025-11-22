9078ad1a6e2352a6adf843ae2eac79d244f49f6a
Borja Marquez de la Plata <bmarquezdelaplata@uc.cl>
2025-11-22 14:12:59 -0300

    fixed cors

--- packages/server/api/app/main.py
+++ packages/server/api/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Enable CORS for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
