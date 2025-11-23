ba5885e55fc648719a38a9282f60ad67806de9bc
CxrlosKenobi <cxrlos@kenobi.dev>
2025-11-23 02:26:29 -0300

    refactor: simplify layout by removing unused font imports and sidebar state management

--- packages/client/app/layout.tsx
+++ packages/client/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Titillium_Web } from "next/font/google";
+import { Titillium_Web } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar, MobileSidebarContent } from "@/components/app-sidebar";
-import { cookies } from "next/headers"
 import { ReduxProvider } from "./providers"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const titilliumWeb = Titillium_Web({
   variable: "--font-titillium-web",
@@ -33,12 +21,9 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   
-  // const cookieStore = await cookies()
-  // const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
-
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${titilliumWeb.variable} antialiased`} >
+      <body className={`${titilliumWeb.variable} antialiased`} >
         <ReduxProvider>
           <SidebarProvider defaultOpen={true}>
             <main className="flex-1 w-full">
