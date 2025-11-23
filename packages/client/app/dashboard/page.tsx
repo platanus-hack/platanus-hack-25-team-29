d43d88082c7781fb56ccc5e4a2cdf1bc5c528115
Borja Marquez de la Plata <bmarquezdelaplata@uc.cl>
2025-11-22 23:20:34 -0300

    feat: removed legacy env

--- packages/client/app/dashboard/page.tsx
+++ packages/client/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default function DashboardPage() {
         const today = new Date()
         const oneYearAgo = format(new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()), "yyyy-MM-dd")
         const todayFormatted = format(new Date(), "yyyy-MM-dd")
-        const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://platanus-grupo29-681510028004.us-central1.run.app'
+        const API_BASE_URL = 'https://platanus-grupo29-681510028004.us-central1.run.app'
         
         const res = await fetch(API_BASE_URL + "/movements?start_date=" + oneYearAgo + "&end_date=" + todayFormatted, {
           method: "GET",
